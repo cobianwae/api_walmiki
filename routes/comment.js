255d0e957c65c9e83b8841df49df3cc2e276c04d
Mochamad Fazar <mochamad.fazar@gmail.com>
2015-01-07 23:45:02 +0700

    add comment test

--- routes/comment.js
+++ routes/comment.js
@@ -2,25 +2,30 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-exports.getByPostId = function(req, res) {	
-	if(isEmptyObject(req.body))
-    return res.status(403).send('Bad Request');
+exports.getByPostId = function(req, res) {			
 	Post.findById(req.params.postId)
 	.populate('comment.author')	
-	.exec(function(err, posts) {
+	.exec(function(err, post) {
 		if (err) {
 			res.send(err);
 		}
 
-		res.send(posts.comment);
+		if (!post) {
+			return res.status(404).send({success:false, message: 'the post is no longer exist'});
+		}      
 
+		res.status(202).send({success: true, comments : post.comment});
 	});
 }
 
-exports.doCreate = function(req, res) {
-	console.log(req.body);
-	if(isEmptyObject(req.body))
+exports.doCreate = function(req, res) {	
+	if(req.body.thePostId === undefined) {
     return res.status(403).send('Bad Request');
+  } else if (req.body.text === undefined ||  req.body.text === '') {
+		res.send({success: false, message: 'you need to write your comment first'});
+		return;
+  }
+  
 
 	Post.findById(req.body.thePostId)
 		.exec(function(err, post) {
--- test/routes/comment.js
+++ test/routes/comment.js
@@ -1,20 +1,70 @@
 describe('Comment API', function() {
 	beforeEach(function(done) {
 		clearDB();
-		done();
+		return done();
 	});
 
-	describe('POST /comments', function(){
-		it('should return error if user trying to comment to unexisted post', function(done) {
-			initializeUser()
-			.then(function(credential) {
-				getUserToken(credential)
-				.then(function(token){
-					request.post('/comments')
-					.set('Authorization', 'Bearer' + token)
-					.expect(403, done)
-				});
-			});
+	describe('Post /comments', function() {
+		it('should return error if user trying to comment to unexisted post', function(done) {			
+			authenticate().then(function(token){
+        request.post('/comments')
+        .set('Authorization', 'Bearer ' + token)
+        .expect(403, done);
+      });
 		});
+
+		it('should return false as success property if user trying to add empty comment', function(done) {			
+			authenticate().then(function(token){
+        request.post('/comments')
+        .set('Authorization', 'Bearer ' + token)
+        .send({thePostId: new mongoose.Types.ObjectId()})
+        .end(function(err, res){
+          res.body.success.should.equal(false);
+          res.body.should.have.property('message');
+          done();
+        });
+      });
+		});
+
+		it('should return 404 and json message if post is no longer exist', function(done) {			
+			authenticate().then(function(token){
+        request.get('/comments/' + new mongoose.Types.ObjectId())	        
+	        .set('Authorization', 'Bearer ' + token)
+	        .expect(404)
+	        .end(function(err, res) {
+	          res.body.success.should.equal(false);
+	          res.body.should.have.property('message');
+	          done();
+        });      	
+      });
+		});
+
+		it('should return 200 and success if comment successfully posted', function(done) {			
+			authenticate().then(function(token) {				
+				request.post('/images')
+				.set('Authorization', 'Bearer ' + token)
+				.attach('image', 'test/resources/sample-image.jpg')
+				.end(function(err, res) {					
+					request.post('/posts')
+          .set('Authorization', 'Bearer ' + token)
+          .send({image: res.body.success[0].id, title: 'My Post For Comment' })
+          .expect(200)
+          .end(function(err, res){
+          	var post = mongoose.model('Post');
+            post.findOne({title: 'My Post For Comment'}, function(err, post) {
+            	request.post('/comments')
+            	.set('Authorization', 'Bearer ' + token)
+            	.send({text: 'my first comment', thePostId: post._id})
+            	.expect(200)
+            	.end(function(err, res) {
+            		 res.body.success.should.equal(true);
+            		 done();
+            	});     	
+          	});
+          });
+				});
+      });
+		});
+
 	});
 })
